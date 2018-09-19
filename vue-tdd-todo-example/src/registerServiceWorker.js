86867122d7cdc78cec9c48f7d56b945d02ef3e73
Danil Chugaev <danilchugaev@mail.ru>
2018-09-19 23:13:23 +0500

    new

--- vue-tdd-todo-example/src/registerServiceWorker.js
+++ vue-tdd-todo-example/src/registerServiceWorker.js
@@ -3,7 +3,7 @@
 import {register} from 'register-service-worker'
 
 if (process.env.NODE_ENV === 'production') {
-  register(`/sw.js`, {
+  register(`/vue-tdd-todo-example/sw.js`, {
     ready () {
       console.log('App is being served from cache by a service worker.\n' +
           'For more details, visit https://goo.gl/AFskqB')
