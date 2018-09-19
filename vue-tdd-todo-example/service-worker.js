3d50e776df138cd3b1d79ddce516eb0f4effc327
Danil Chugaev <danilchugaev@mail.ru>
2018-09-19 23:14:14 +0500

    new

--- vue-tdd-todo-example/service-worker.js
+++ vue-tdd-todo-example/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.2.0/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.765554f43ce4db8c142773adc57058cf.js"
+  "/precache-manifest.86a2329016f27f4894a64b7e9c385c8b.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "vue-tdd-todo"});
